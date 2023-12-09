a2d78dbf5ef6bfd2ca747b0772b69b7dbc2a3830
Next Alone <12210746+NextAlone@users.noreply.github.com>
2023-12-10 03:03:13 +0800

    feat: prepare in-app update
    Signed-off-by: Next Alone <12210746+NextAlone@users.noreply.github.com>

--- uploadCI.py
+++ uploadCI.py
@@ -3,13 +3,15 @@ import os
 import requests
 
 apiAddress = "http://127.0.0.1:8081/"
-urlPrefix = apiAddress + "bot" + os.getenv("TELEGRAM_TOKEN")
+urlPrefix = apiAddress + "bot" + os.getenv("TELEGRAM_TOKEN", "")
+
 
 def findString(sourceStr, targetStr):
     if str(sourceStr).find(str(targetStr)) == -1:
         return False
     else:
         return True
+
 
 def genFileDirectory(path):
     files_walk = os.walk(path)
@@ -21,17 +23,58 @@ def genFileDirectory(path):
 
     return target
 
+
+def sendMetadataDesc():
+    parma = {
+        "chat_id": os.environ.get("METADATA_CHANNEL_ID"),
+        "text": os.environ["COMMIT_MESSAGE"],
+    }
+    response = requests.post(urlPrefix + "/sendMessage", params=parma)
+
+    return response.json()["result"]["message_id"]
+
+
 def sendAPKs(path):
     file = genFileDirectory("./apks")
 
     parma = {
         "chat_id": os.environ.get("CHAT_ID"),
-        "caption": "New Nnngram build. Congratulations!\n\nVersion: " + os.environ.get("VERSION_NAME") + "(" + os.environ.get("VERSION_CODE") + ")" + "\n\nCommit message:\n" + os.environ.get("COMMIT_MESSAGE"),
+        "caption": "New Nnngram build. Congratulations!\n\nVersion: "
+        + os.environ.get("VERSION_NAME", "")
+        + "("
+        + os.environ.get("VERSION_CODE", "")
+        + ")"
+        + "\n\nCommit message:\n"
+        + os.environ.get("COMMIT_MESSAGE", ""),
     }
 
     print(parma)
 
     r = requests.post(urlPrefix + "/sendDocument", params=parma, files=file)
 
+    parma["chat_id"] = os.environ.get("APK_CHANNEL_ID")
+
+    r = requests.post(urlPrefix + "/sendDocument", params=parma, files=file)
+
+    return r.json()["result"]["message_id"] - 1
+
+
+def sendMetadata(changesID, startID):
+    parma = {
+        "chat_id": os.environ.get("METADATA_CHANNEL_ID"),
+        "text": str(os.getenv("VERSION_NAME"))
+        + ","
+        + str(os.getenv("VERSION_CODE"))
+        + ","
+        + str(startID)
+        + ","
+        + str(changesID)
+        + ",false",
+    }
+    response = requests.get(urlPrefix + "/sendMessage", params=parma)
+
+
 if __name__ == "__main__":
+    changesID = sendMetadataDesc()
     startID = sendAPKs("./apks")
+    sendMetadata(changesID, startID)
